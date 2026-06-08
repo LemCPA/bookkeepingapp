edb1469a4c91b58b37bc09ccffb7046f5400c6e6
Ted <ted@lemcpa.ca>
2026-06-07 23:03:38 -0400

    Update About page to use Cloudinary hosted video URL

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,7 +24,7 @@ export default function AboutPage() {
               className="w-full h-full"
               poster="/video-poster.jpg"
             >
-              <source src="/videos/BKA-Intro.mp4" type="video/mp4" />
+              <source src="https://res.cloudinary.com/dgnxufaol/video/upload/v1780887696/BKA_Intro_ckoedz.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
